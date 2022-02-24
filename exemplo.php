e57845ad954c3e14393b88100c24bccbc0596236
Gabriele Batagiero <79870386+GabrieleBatagiero@users.noreply.github.com>
2022-02-24 08:56:35 -0300

    Add files via upload

---
+++ exemplo.php
@@ -0,0 +1,19 @@
+<?php
+    $numero = 10;
+    
+    $outro_numero = 10;
+
+    var_dump($numero);
+    echo "<br>"
+    var_dump($outro_numero);
+    echo "<br>"
+
+    if ($numero === $outro_numero){
+        echo "valores iguais";
+    }
+        else{
+            echo "são valores diferentes";
+        }
+    
+
+?>
